601cddf5053801bb7300da1af7cc8f9d66faef8e
Jaayden Halko <jaayden.halko@gmail.com>
2018-03-03 17:32:45 +0700

    Update leaderboard player design

---
+++ Assets/Fourzy/Scripts/Common/LoadingManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Fourzy
+{
+    public class LoadingManager : Singleton<LoadingManager>
+    {
+        public void LoadNextScene()
+        {
+            int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(activeSceneIndex + 1);
+        }
+
+        public void LoadPreviousScene()
+        {
+            int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(activeSceneIndex - 1);
+        }
+    }
+}
