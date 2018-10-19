633147eb4fa4be3a82fb7d92194d605f09dfc351
Oleksiy Fedosov <zorrendor@gmail.com>
2018-10-19 16:19:25 +0700

    Optimize UI for tablets

---
+++ Assets/Fourzy/Scripts/UI/SpriteScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScaler : MonoBehaviour
+{
+    private void Awake()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        float standardAspectRatio = 750f / 1334f;
+        if (aspectRatio > standardAspectRatio)
+        {
+            Vector3 scale = this.transform.localScale;
+            scale = scale * (standardAspectRatio / aspectRatio);
+            this.transform.localScale = scale;
+        }
+    }
+}
