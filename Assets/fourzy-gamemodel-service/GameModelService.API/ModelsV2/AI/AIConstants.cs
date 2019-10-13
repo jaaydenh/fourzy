fb16f591c57995f8e518c9b0aa312cec7a5d150b
Jaayden Halko <jhalko@automotivemastermind.com>
2019-10-13 11:06:49 -0400

    updates to map levels

--- Assets/fourzy-gamemodel-service/GameModelService.API/ModelsV2/AI/AIConstants.cs
+++ Assets/fourzy-gamemodel-service/GameModelService.API/ModelsV2/AI/AIConstants.cs
@@ -16,30 +16,30 @@ namespace FourzyGameModel.Model
     }
 
     public enum AIProfile { Player,
-        PuzzleAI,
-        SimpleAI,
-        PassBot,
-        PositionBot,
-        BossAI,
-        BeginnerAI,
-        EasyAI,
-        BetterAI,
-        ScoreBot,
-        UpBot,
-        RotatorBot,
-        HorizontalBot,
-        VerticalBot,
-        WaitBot,
-        ExtenderBotAI,
-        DownAI,
-        PanicBot,
-        RightAI,
-        LeftAI,
-        DoctorBot,
-        BlindBot,
-        BadBot,
-        SmartBot,
-        AggressiveAI
+        PuzzleAI,       // 1
+        SimpleAI,       // 2
+        PassBot,        // 3
+        PositionBot,    // 4
+        BossAI,         // 5
+        BeginnerAI,     // 6
+        EasyAI,         // 7
+        BetterAI,       // 8
+        ScoreBot,       // 9
+        UpBot,          // 10
+        RotatorBot,     // 11
+        HorizontalBot,  // 12
+        VerticalBot,    // 13
+        WaitBot,        // 14
+        ExtenderBotAI,  // 15
+        DownAI,         // 16
+        PanicBot,       // 17
+        RightAI,        // 18
+        LeftAI,         // 19
+        DoctorBot,      // 20
+        BlindBot,       // 21
+        BadBot,         // 22
+        SmartBot,       // 23
+        AggressiveAI    // 24
     }
 
     public enum AIDifficulty { Pushover, Easy, Medium, Hard, Doctor}
