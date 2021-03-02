56bedc3ff92e82591c63f7ff2797234573d95f78
Jaayden Halko <jaayden.halko@gmail.com>
2021-03-02 19:13:04 +0000

    Use SystemInfo.deviceUnityIdentifier for iOS

--- Assets/Fourzy/Scripts/Managers/DeviceIDManager.cs
+++ Assets/Fourzy/Scripts/Managers/DeviceIDManager.cs
@@ -6,22 +6,10 @@ public static class DeviceIDManager
 {
     public static Action<string> onDeviceID;
 
-
-#if UNITY_IPHONE && !UNITY_EDITOR
-    [DllImport("__Internal")]
-    static extern string _Get_Device_id();
-#endif
-
     public static void GetDeviceID()
     {
         string password = string.Empty;
-
-#if UNITY_IPHONE && !UNITY_EDITOR
-		password = _Get_Device_id();
-        onDeviceID?.Invoke(password);
-#else
         password = SystemInfo.deviceUniqueIdentifier;
         onDeviceID?.Invoke(password);
-#endif
     }
 }
