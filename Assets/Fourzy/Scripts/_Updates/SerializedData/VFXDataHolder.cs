15a6f16b2cd7a7cd8d0f4490e066ffd2bec2ff81
gilett4 <vadymudod@hotmail.com>
2021-04-09 12:29:01 -0700

    spawn vfx added (#18)
    use GAMEPIECE_AFTER_SPAWN_DELAY to control when new gamepiece starts moving

--- Assets/Fourzy/Scripts/_Updates/SerializedData/VFXDataHolder.cs
+++ Assets/Fourzy/Scripts/_Updates/SerializedData/VFXDataHolder.cs
@@ -4,45 +4,25 @@ using Fourzy._Updates.Mechanics._Vfx;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using StackableDecorator;
 
 namespace Fourzy._Updates.Serialized
 {
     [CreateAssetMenu(fileName = "DefaultVFXHolder", menuName = "Create VFX Data Holder")]
     public class VFXDataHolder : ScriptableObject
     {
-        [List]
-        public VfxNameList data;
-    }
-
-    [Serializable]
-    public class VfxNameList
-    {
-        public List<VfxNamePair> list;
+        public List<VfxNamePair> data;
     }
 
     [Serializable]
     public class VfxNamePair
     {
-        [HideInInspector]
-        public string _name;
-
-        [ShowIf("#Check")]
-        [StackableField]
-        public VfxType type;
+        public string type;
         public Vfx[] options;
-
-        public bool Check()
-        {
-            _name = type.ToString();
-
-            return true;
-        }
     }
 
     public class VfxPool
     {
-        public VfxType type;
+        public string type;
         public List<Vfx>[] pool;
     }
 }
