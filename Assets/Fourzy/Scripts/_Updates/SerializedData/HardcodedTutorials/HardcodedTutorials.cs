4c2cc984aa993bfcdbd89f4dafa46f5f55eeebe8
Jaayden Halko <jaayden.halko@gmail.com>
2021-04-19 23:26:51 +0100

    updates for FTUE

--- Assets/Fourzy/Scripts/_Updates/SerializedData/HardcodedTutorials/HardcodedTutorials.cs
+++ Assets/Fourzy/Scripts/_Updates/SerializedData/HardcodedTutorials/HardcodedTutorials.cs
@@ -125,17 +125,16 @@ namespace Fourzy._Updates._Tutorial
                     new OnboardingTask_Wait(1f),
                     new OnboardingTask() { action = OnboardingActions.LOAD_MAIN_MENU },
 
+                    new OnboardingTask_Log("highlightPlayButton"),
+                    //point at play button
+                    new OnboardingTask_HighlightButton(
+                        "OnlineButton",
+                        "MainMenuCanvas",
+                        Vector2.one,
+                        Vector2.zero,
+                        new Vector2(100, -23f),
+                        new MessageData(LocalizationManager.Value("tap_to_play_first_game"), Vector2.zero, new Vector2(0f, -210f))),
                     new OnboardingTask_Log("tutorialCompleted"),
-                    // //point at adventure button button
-                    // new OnboardingTask_Log("5"),
-                    // new OnboardingTask_HighlightButton(
-                    //     "AdventureButton",
-                    //     "MainMenuCanvas",
-                    //     Vector2.one,
-                    //     Vector2.zero,
-                    //     Vector2.zero,
-                    //     new MessageData(LocalizationManager.Value("tap_to_open_map"), Vector2.zero, new Vector2(0f, -165f))),
-                    // new OnboardingTask_Log("6"),
 
                     // new OnboardingTask_HighlightProgressionEvent (
                     //     0,
