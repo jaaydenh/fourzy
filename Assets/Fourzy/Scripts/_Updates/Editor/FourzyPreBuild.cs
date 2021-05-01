3c6d0e694490cfdce565dd8dedef1ff7a20a77d4
gilett4 <vadymudod@hotmail.com>
2021-05-01 11:00:31 +0300

    resourcedb update on preinstall

---
+++ Assets/Fourzy/Scripts/_Updates/Editor/FourzyPreBuild.cs
@@ -0,0 +1,23 @@
+//@vadym udod
+
+#if UNITY_EDITOR
+
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+class FourzyPreBuld : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        ResourceDB _instance = ResourceDB.Instance;
+
+        Debug.Log($"Before update: files, {_instance.FileCount} folders {_instance.FolderCount}");
+        ResourceDB.Instance.UpdateDB();
+        Debug.Log("---- Resource DB updated");
+    }
+}
+
+#endif
